cf94bbf252c6b962e1a9b59128e27ffeae1f0d9a
imad <imadatyatalah@gmail.com>
2021-03-06 15:18:57 +0100

    Added lang="en" to the head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
+        <Head lang="en">
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
