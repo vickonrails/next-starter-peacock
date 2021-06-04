88b9c008e75d9c79193fe72be11a7f60ab633221
vickonrails <victorofoegbu0009@gmail.com>
2021-06-04 10:09:24 +0100

    feat: add plausible analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,15 @@ export default class MyDocument extends Document {
             title={site.siteName}
             href={`${site.siteUrl}rss.xml`}
           />
+
+          {/* Add plausible analytics */}
+          <script
+            async
+            defer
+            data-domain="victorofoegbu.com"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
